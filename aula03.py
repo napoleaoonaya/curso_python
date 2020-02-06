9f7f27c0aafcc138f5eb054f6c31dc3978b99a5a
napoleaoonaya <napoleao.onaya@gmail.com>
2020-02-05 22:53:46 -0300

    Curso de python

---
+++ aula03.py
@@ -0,0 +1,13 @@
+print("Operadores aritmeticos")
+print("Soma")
+print 1+2
+print("Subtracao")
+print 3-1
+print("Divisao")
+print 10/2
+print("Divisao inteira")
+print 10//3
+print("Modulo")
+print 10%3
+print("Potencia")
+print 2**8
