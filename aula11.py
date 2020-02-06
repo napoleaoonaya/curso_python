477ee2a8d510a9e0a18f4dc1866770e976015862
napoleaoonaya <napoleao.onaya@gmail.com>
2020-02-06 00:04:09 -0300

    Funcao startswith(n) e endswith(o)

--- aula11.py
+++ aula11.py
@@ -1,5 +1,5 @@
 st = "napoleao"
-print("Comeca com a letra n")
+print("Comeca com a letra n a string")
 print st.startswith("n")
-print("Termina com a letra o")
+print("Termina com a letra o a string")
 print st.endswith("o")
