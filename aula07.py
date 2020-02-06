9b78a76f9e73de7b0cfb4d6accd87987bae1c994
napoleaoonaya <napoleao.onaya@gmail.com>
2020-02-05 23:38:18 -0300

    Validacao logica de string por comparacao por in e not in, concatenacao, e multiplicacao de caracter

---
+++ aula07.py
@@ -0,0 +1,10 @@
+print("pela string ser um vetor a mesma suporta uma varedura com condicionais logicas")
+st = "napoleao"
+print("A letra n esta na variavel")
+print "n" in st
+print("A letra x nao esta na variavel")
+print "x" not in st
+print("Concatenando string")
+print "n" + "apoleao"
+print("Multiplicando um caracter")
+print "a"*3
