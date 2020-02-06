611e7a5054597eef9553d5b168fd828b7dccf49d
napoleaoonaya <napoleao.onaya@gmail.com>
2020-02-05 23:01:45 -0300

    Curso de python

---
+++ aula04.py
@@ -0,0 +1,14 @@
+print("Operadores de bits")
+print("ou")
+print 1|0
+print 1|5
+print("exclusivo")
+print 1^5
+print("e")
+print 4&1
+print("bit deslocado a esquerda")
+print 1<<2
+print("bit deslocado a direita")
+print 4>>2
+print("inverso em bits")
+print ~4
