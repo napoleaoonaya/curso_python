797a53a85f976c7be51d164c9104090fb891c473
napoleaoonaya <napoleao.onaya@gmail.com>
2020-02-05 23:29:02 -0300

    String divida por indices usando slice, funcao len que mostra o total de caracteres de uma string

---
+++ aula06.py
@@ -0,0 +1,8 @@
+print("No python toda string e considerada um vetor sendo assim cada letra tem uma posicao representado pelo seu indice")
+st = "napoleao"
+print("uso de slices são separacao de letra pela posicoes")
+print st[1:4]
+print st[2:]
+print st[:3]
+print("len conta quantas palavras tem uma string")
+print len(st)
